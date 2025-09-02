bf1fef4fcb13a48d8332a6db7df59b9db741c483
higorladim <149248211+higorladim@users.noreply.github.com>
2025-09-01 22:41:54 -0300

    solved using while

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function (a, b) {
+    
+    if (b == 0) {
+        return "";
+    } else if (b < 0){
+        return "ERROR";
+    } else {
+        
+        let count = 0;
+        let newString = "";
 
+        while (count < b) {
+            newString += a;
+            count++;
+        }
+        return newString;
+    }
 };
 
 // Do not edit below this line
