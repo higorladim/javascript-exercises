8d88bec5c12f01bb8d712509cb6723d5e2724baf
higorladim <149248211+higorladim@users.noreply.github.com>
2025-09-02 12:22:50 -0300

    test 1 success

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(name) {
+    let count = 0;
+    let strSize = name.length;
+    let strReversed = "";
 
+    while (count <= strSize){
+        let a = name.charAt(strSize);
+        console.log("a = ", a);
+        strReversed += a;
+        console.log("strReversed = ", strReversed);
+        strSize--;
+        console.log("strSize = ", strSize);
+    }
+    return strReversed;
 };
 
 // Do not edit below this line
