f094548b3f806993e0c2b5d46da7f5e73603a201
higorladim <149248211+higorladim@users.noreply.github.com>
2025-09-03 22:20:38 -0300

    Test 1 and 2 passed git add .

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    console.log("theArgs: ", theArgs);
+    let newArray;
+    for (const arg of theArgs){   
+
+        console.log("arg: ", arg);
+        console.log("array: ", array);
+        
+        if (arg > -1){
+        index = array.indexOf(arg);
+        console.log("index: ", index);
+        array.splice(index, 1);
+        }
+    }
+    
+    console.log("array: ", array);
+    return array;
+    
 };
 
 // Do not edit below this line
