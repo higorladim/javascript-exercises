1330dbb43e0561511e1a4c8723ff6f6cb0ca5fcf
higorladim <149248211+higorladim@users.noreply.github.com>
2025-09-08 22:18:39 -0300

    Challenge completed!

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,45 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let sumAll = 0;
+    const err = "ERROR";
+
+    if (typeof num1 === "string" ||
+        num1 < 0 ||
+        num1 % 1 !== 0) {
+        return err;
+
+    } else if (typeof num2 === "string" ||
+        num2 < 0 ||
+        num2 % 1 !== 0) {
+        return err;
+    }
+
+    if (num1 > num2) {
+        sumAll = num1;
+        for (let i = 0; num1 > num2; i++) {
+            sumAll = sumAll + num2;
+            num2++;
+        };
+
+        if (sumAll) {
+            return sumAll;
+        } else {
+            return err;
+        }
+    }
+
+    if (num1 < num2) {
+        sumAll = num2;
+        for (let i = 0; num1 < num2; i++) {
+            sumAll = sumAll + num1;
+            num1++;
+        };
+
+        if (sumAll) {
+            return sumAll;
+        } else {
+            return err;
+        }
+    }
 
 };
 
