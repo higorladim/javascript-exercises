72466429baef41d2f3fc101ccfaa023bdda82976
higorladim <149248211+higorladim@users.noreply.github.com>
2025-09-10 20:29:52 -0300

    all testes completed

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,32 +1,49 @@
-const add = function(num1, num2) {
-	const result = num1 + num2;
+const add = function (num1, num2) {
+  const result = num1 + num2;
   return result;
 };
 
-const subtract = function(num1, num2) {
-	const result = num1 - num2;
+const subtract = function (num1, num2) {
+  const result = num1 - num2;
   return result;
 };
 
-const sum = function(array) {
+const sum = function (array) {
   let result = 0;
   const numbers = array;
-	numbers.forEach(element => {
+  numbers.forEach(element => {
     result += element;
   });
   return result;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let result = 1;
+  const numbers = array;
+  numbers.forEach(element => {
+    result *= element;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  const result = num1 ** num2;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result; 
+  if (num === 0 || num === 1) { 
+    console.log("caiu no if"); 
+    return 1; 
+  } else if (num < 0) { 
+    return 1; 
+  } else {
+     console.log("caiu no else");
+      for (i = num - 1; i > 1; i--) { 
+        num = num * i; 
+      } 
+    } return num;
 };
 
 // Do not edit below this line
