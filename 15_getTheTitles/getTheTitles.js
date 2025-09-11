b8f69a43b1ef2b1a2348f1e11a2088d4c294d0fd
higorladim <149248211+higorladim@users.noreply.github.com>
2025-09-11 20:32:08 -0300

    Solved using map

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const bookshelf = books;
+    const titles = bookshelf.map(({title}) => title);
+    console.log ("titles: ", titles);
+    return titles;
 };
 
 // Do not edit below this line
