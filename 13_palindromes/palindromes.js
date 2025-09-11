8674892fd2509a1b204e9b5dd2ee2e9f26a228b3
higorladim <149248211+higorladim@users.noreply.github.com>
2025-09-11 19:34:10 -0300

    First test okay

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringReversed = reverse(string);
+    console.log("stringReversed: ", stringReversed);
+
+    if (stringReversed === string){
+        return true;
+    } else {
+        return false;
+    }
 
 };
+
+function reverse(s){
+    return s.split("").reverse().join("");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
